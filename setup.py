fc6f973f25223a8e8538f9d5205f903f0ba498ff
Pablo Virgo <mailbox@pablovirgo.com>
2018-06-26 23:42:19 -0400

    setup.py up to date

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 import os
 from setuptools import find_packages, setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst'), 'r') as readme:
+with open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r') as readme:
     README = readme.read()
 
 setup(
     name='happiness_star',
     description='Django app for tracking life satisfaction or happiness',
     long_description=README,
-    version='0.1.0',
+    version='0.2.0',
     author='Pablo Virgo',
     author_email='mailbox@pablovirgo.com',
     packages=find_packages()
